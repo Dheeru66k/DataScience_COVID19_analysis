c7aa083f69f0c3aded9c30b4cdbfc3045ac19874
Dheeru66k <Dheeru66k@users.noreply.github.com>
2020-09-14 14:01:37 +0200

    Initial import.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science for COVID-19 analysis',
+    author='Deeraj Kumar Kanala',
+    license='',
+)
